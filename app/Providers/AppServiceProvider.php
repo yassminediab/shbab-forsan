9d5b5b4141ce24ef883ce4f5ced2ab8da68a985f
Omar Moustafa <omarmoustafa@Omars-MacBook-Pro.local>
2021-03-10 01:13:49 +0200

    speed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        View::composer('*', FooterComposer::class);
+        View::composer('frontend.layouts.header', FooterComposer::class);
     }
 }
